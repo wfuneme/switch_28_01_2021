e291c7f8339fc7c43fc6195585bd19bba2d2e970
Willmer Obed Fúneme San Martín <willmer.funeme@unac.edu.co>
2021-01-28 18:58:06 -0500

    Switch

---
+++ main.js
@@ -0,0 +1,22 @@
+// let key = parseInt(prompt("Ingrese una "));
+
+let key = prompt("Ingrese una opción ");
+switch (key) {
+    case "1":
+        console.log(`Opción uno`);
+        break;
+    case "2":
+        console.log(`Opción uno tipo`);
+        break;
+    default:
+        console.log(`Opción incorrecta`);
+        break;
+}
+
+//Funciones anónimas 
+// let saludar = function saludar(params) {
+//     return `hola ${params}`;
+// }
+// console.log(saludar("Willmer"));
+
+
